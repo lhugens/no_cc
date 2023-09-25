67f27a4ee364a3a619a3dc4a5f36f8cf2340c935
hugens <hugens@localhost.localdomain>
2023-09-25 21:28:46 +0100

    implementation works

--- my_functions.c
+++ my_functions.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>  // Include the header for malloc
+#include <stdlib.h>  // malloc
 
 struct Tuple{
     int first;
@@ -9,10 +9,15 @@ struct Tuple{
 void dfs(int k1, int visited[], int V[], int lengthV, struct Tuple *E, int lengthE){
     visited[k1] = 1;
     for(int e=0; e<lengthE; e++){
-        int from = E[e].first-1;
-        if(from == k1){
+        if(E[e].first-1 == k1){
             int p = E[e].second-1; 
             if(visited[p] == 0){
+                dfs(p, visited, V, lengthV, E, lengthE);
+            }
+        }
+        if (E[e].second - 1 == k1) { 
+            int p = E[e].first - 1;
+            if (visited[p] == 0) {
                 dfs(p, visited, V, lengthV, E, lengthE);
             }
         }
@@ -20,10 +25,9 @@ void dfs(int k1, int visited[], int V[], int lengthV, struct Tuple *E, int lengt
 }
 
 int my_no_connected_components(int V[], int lengthV, struct Tuple* E, int lengthE){
-    // init
     int ncc = 0;
 
-        // create visited array
+    // create visited array
     int *visited = malloc(lengthV * sizeof(int)); // Allocate memory for the visited array
     if (visited == NULL) { // Handle memory allocation error if necessary
         return -1; // Indicate an error
@@ -31,18 +35,12 @@ int my_no_connected_components(int V[], int lengthV, struct Tuple* E, int length
     for(int i=0; i<lengthV; i++){
         visited[i] = 0;
     }
-    //
-    // find
+    // 
     for(int k=0; k<lengthV; k++){
         if(visited[k] == 0){
             dfs(k, visited, V, lengthV, E, lengthE);
             ncc++;
         }
     }
-    //int sum = 0;
-    //for(int i=0; i<lengthV; i++){
-    //    sum += visited[i];
-    //}
-
     return ncc;
 }
